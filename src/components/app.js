d9a04b225462fba5322151657a875bd2dfb1833d
Koy Herrick <herrickoy@gmail.com>
2020-08-25 12:32:12 -0600

    added clip path and colors along with it

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,14 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <BirthdayFrom />
+        <header>
+          <h1>Birthday Countdown</h1>
+          <div className='header__skew'>
+            <div className='header__subskew'>
+              <BirthdayFrom />
+            </div>
+          </div>
+        </header>
       </div>
     );
   }
