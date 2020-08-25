eded1c87a89d7348bb22ba7d9b8b0bbe139d4161
Koy Herrick <herrickoy@gmail.com>
2020-08-25 13:54:39 -0600

    added mixin page and happy birthday message styles

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -104,7 +104,10 @@ export default class Clock extends Component {
             <div className="countdown">
             {
                 this.state.timeRemaining == 0 ?
-                    <h1>Happy Birthday!</h1>
+                    <div className="message-container">
+                        <p className="message-container__title">Countdown Complete</p>
+                        <p className="message-container__message">HAPPY BIRTHDAY!</p>
+                    </div>
                 :
                     <div>
                         <div>
