4986c34c6a7afc1085c0e6386852afe911bdc3a4
Koy Herrick <herrickoy@gmail.com>
2020-08-24 19:01:52 -0600

    set up render message and no birth year fix

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -11,6 +11,7 @@ export default class Clock extends Component {
         this.timer = 0
         this.birthday = props.birthdayFormState.startDate.toString();
         this.getTimeRemaining = this.getTimeRemaining.bind(this);
+        this.noBirthYear = new Date(this.birthday).getFullYear() == new Date().getFullYear();
     }
 
     getTimeRemaining(birthday) {
@@ -85,6 +86,17 @@ export default class Clock extends Component {
         clearInterval(this.timer);
     }
 
+    renderMessage = function () {
+        if(this.noBirthYear) {
+            return (
+                <h4>until your birthday!</h4>
+            )
+        }
+        return (
+            <h4>remaining until you are {this.getAge()}</h4>
+        )
+    }.bind(this);
+
     render() {
         const data = this.state.timeRemaining
         
@@ -102,7 +114,7 @@ export default class Clock extends Component {
                             <div>SECS {data.seconds}</div>
                         </div>
                         <div>
-                            {<h4>remaining until you are {this.getAge()}</h4>}
+                            {this.renderMessage()}
                         </div>
                     </div>
 
